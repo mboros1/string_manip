c61232bd9bc41a47b8aa772e2c20264154b10e87
Martin Boros <mboros04@gmail.com>
2024-07-20 22:42:31 -0400

    renae str_manip to str_len_test

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc-14 str_manip.c -O3 -I$(brew --prefix simde)/include/ -flax-vector-conversions
+gcc-14 str_len_test.c -O3 -I$(brew --prefix simde)/include/ -flax-vector-conversions
